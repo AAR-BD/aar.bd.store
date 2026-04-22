6842cbb65665deeb50d2167585f6d8007637914f
AAR-BD <aar.bd.official@gmail.com>
2026-04-22 15:44:32 +0600

    Update logo.tsx with new AAR logo component - 2026-04-22 09:10:14

--- components/icons/logo.tsx
+++ components/icons/logo.tsx
@@ -1,4 +1,14 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="100" height="50" viewBox="0 0 100 50">
-  <text x="0" y="35" font-family="Arial" font-size="35" fill="white">AA</text>
-  <text x="30" y="35" font-family="Arial" font-size="35" fill="red">R</text>
-</svg>
+import React from 'react';
+
+const AARLogo: React.FC = () => {
+    return (
+        <svg width="200" height="100" viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg">
+            <rect width="100%" height="100%" fill="white" />
+            <circle cx="50" cy="50" r="30" fill="red" />
+            <polygon points="100,10 130,90 70,90" fill="teal" />
+            <rect x="150" y="30" width="40" height="40" fill="red" />
+        </svg>
+    );
+};
+
+export default AARLogo;
